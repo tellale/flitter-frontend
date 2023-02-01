bdadb41c482553a97a32968a7acee612da723e4e
unknown <elisabet.v.alvarez@gmail.com>
2023-02-01 15:26:11 +0100

    styles changes added

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,7 +4,6 @@
   <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
     <div class="mb-4">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-        
       </label>
       <input
         class="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
