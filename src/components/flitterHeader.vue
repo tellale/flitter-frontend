cfe8e90b24a9684a92e31eebe11f0d265d4f86e5
tellale <alejandraterego@gmail.com>
2023-02-06 17:14:02 +0100

    add writeTweet component and functionality. Missing postedBy, avatar,  and Likes config

---
+++ src/components/flitterHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="pt-5 px-8 pb-3 border-b border-lighter">
+        <img class="mx-auto w-6 h-6" src="@/assets/logo_fitter_1.png">
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'flitterHeader'
+
+})
+</script>
