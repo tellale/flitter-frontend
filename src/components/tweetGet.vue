dfd1a9c639077080a62bb063f0dbf559fb39c474
Isela Alarcón <isela.v.alarcon@gmail.com>
2023-02-08 17:09:03 +0100

    fix the tweet mapping

--- src/components/tweetGet.vue
+++ src/components/tweetGet.vue
@@ -1,66 +1,67 @@
 <template>
-        <div v-for="tweet in tweets" :key="tweet._id" class="w-full p-4 border-b hover:bg-ligther flex">
-            <p>{{ tweets }}</p>
-            <div v-for="tw in tweet" :key="tw._id">
-                
+  <div
+    v-for="tweet in tweets"
+    :key="tweet._id"
+    class="w-full p-4 border-b hover:bg-ligther flex"
+  >
+    <p>{{ tweet.text }}</p>
+    <div class="flex-none mr-4">
+      <!-- <img src="``" class="h-12 w-12 rounded-full flex-none"> -->
+    </div>
+    <div class="w-full">
+      <div class="flex items-center w-full">
+        <p class="font-semibold">{{ tweet.postedBy.name }}</p>
+        <!-- <p class="text-sm text-dark ml-2">{{ follow.handle }}</p>-->
+        <p
+          class="text-sm text-lightblue ml-2"
+          v-for="tag in tweet.tags"
+          :key="tag"
+        >
+          {{ tag }}
+        </p>
+      </div>
+      <p class="py-3">{{ tweet.text }}</p>
 
-            </div>
-            <div class="flex-none mr-4">
-                <!-- <img src="``" class="h-12 w-12 rounded-full flex-none"> -->
-            </div>
-            <div class="w-full">
-                <div class="flex items-center w-full">
-                    <p class="font-semibold">{{ tweet.postedBy }}</p>
-                    <!-- <p class="text-sm text-dark ml-2">{{ follow.handle }}</p>-->
-                    <p class="text-sm text-lightblue ml-2">{{ tweet.tags }}</p>
-                </div>
-                <p class="py-3">{{ tweet.text }}</p>
-                
-                <div class="flex items-center justify-between w-full">
-                    <div class="flex items-center text-sm text-grey hover:text-lightblue">
-                        <font-awesome-icon icon="fa-regular fa-comment" class="mr-3" />
-                    </div>
-                    <div class="flex items-center text-sm text-grey hover:text-lightblue">
-                        <font-awesome-icon icon="fa-solid fa-retweet" class="mr-3" />
-                    
-                    </div>
-                    <div class="flex items-center text-sm text-grey hover:text-lightblue">
-                        <font-awesome-icon icon="fa-regular fa-heart" class="mr-3" />
-                       
-                    </div>
-                </div>
-            </div>
+      <div class="flex items-center justify-between w-full">
+        <div class="flex items-center text-sm text-grey hover:text-lightblue">
+          <font-awesome-icon icon="fa-regular fa-comment" class="mr-3" />
         </div>
+        <div class="flex items-center text-sm text-grey hover:text-lightblue">
+          <font-awesome-icon icon="fa-solid fa-retweet" class="mr-3" />
+        </div>
+        <div class="flex items-center text-sm text-grey hover:text-lightblue">
+          <font-awesome-icon icon="fa-regular fa-heart" class="mr-3" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-    import { computed, onMounted } from 'vue';
-    import { useTweetsStore } from '../store/index';
+import { computed, onMounted } from "vue";
+import { useTweetsStore } from "../store/index";
 
-    export default {
-        name: 'tweetGet',
-        setup() {
-            const store = useTweetsStore();
+export default {
+  name: "tweetGet",
+  setup() {
+    const store = useTweetsStore();
 
-            // const getTweets = computed(() => {
-            //     return store.getTweets;
-            //     })
-            
-            const tweets = computed(() => {
-                return store.tweets;
-            })
+    // const getTweets = computed(() => {
+    //     return store.getTweets;
+    //     })
 
-            onMounted(() => {
-                store.fetchTweets()
-            })
+    const tweets = computed(() => {
+      return store.tweets;
+    });
 
-            return {
-                //getTweets,
-                tweets
-            }
+    onMounted(() => {
+      store.fetchTweets();
+    });
 
-        },
-
-    }
-
+    return {
+      //getTweets,
+      tweets,
+    };
+  },
+};
 </script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,43 +1,49 @@
 "use strict";
 import { defineStore } from "pinia";
 import axios from "axios";
-import Tweet from '@/interfaces/Tweets'
+import Tweet from "@/interfaces/Tweets";
 
-export const useTweetsStore = defineStore('tweets', {
-    state: () => ({
-        tweets: [] as Tweet[],
-        isLoading: false,
-    }),
-    getters: {
-        getTweets(state) {
-            return state.tweets
-        },
+export const useTweetsStore = defineStore("tweets", {
+  state: () => ({
+    tweets: [] as Tweet[],
+    isLoading: false,
+  }),
+  getters: {
+    getTweets(state) {
+      return state.tweets;
     },
-    actions: {
-        async fetchTweets() {
-            try {
-                const data = await axios.get('http://localHost:3000/api/tweet?page=0&limit=20')
-                this.tweets = data.data
-            } catch (err) {
-                alert(err)
-                console.log(err)
-            }
-        },
-
-        async writeTweet(tweet: string) {
-            const data = await axios.post('http://localHost:3000/api/create-tweet', tweet)
-            this.tweets.push(data.data)
-        },
-
-        async fetchUserTweets(userId: number | undefined) {
-            try {
-                const data = await axios.get(`http://localHost:3000/api/tweet/user/${userId}`)
-                this.tweets = data.data
-            } catch (err) {
-                alert(err)
-                console.log(err)
-            }
-        }
+  },
+  actions: {
+    async fetchTweets(page = 0, limit = 20) {
+      try {
+        const res = await axios.get(
+          `http://localhost:3000/api/tweet?page=${page}&limit=${limit}`
+        );
+        this.tweets = res.data[0].tweets;
+      } catch (err) {
+        alert(err);
+        console.log(err);
+      }
     },
-})
 
+    async writeTweet(tweet: string) {
+      const data = await axios.post(
+        "http://localhost:3000/api/create-tweet",
+        tweet
+      );
+      this.tweets.push(data.data);
+    },
+
+    async fetchUserTweets(userId: number | undefined) {
+      try {
+        const data = await axios.get(
+          `http://localhost:3000/api/tweet/user/${userId}`
+        );
+        this.tweets = data.data;
+      } catch (err) {
+        alert(err);
+        console.log(err);
+      }
+    },
+  },
+});
