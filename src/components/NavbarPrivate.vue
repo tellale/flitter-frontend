d73a3ef98c5aa311d7b98365000c8c43e7ede286
Behbiz <isabelcastrobcn@gmail.com>
2023-02-08 19:39:15 +0100

    create NavbarPrivate.vue and NavbarPublic.vue

---
+++ src/components/NavbarPrivate.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="py-3 px-3 flex justify-between items-center">
+      <router-link to="/">
+        <icon name="home"/>
+      </router-link>
+      <icon name="search"/>
+      <icon name="sign-out-alt"/>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+
+  };
+  </script>
---
+++ src/components/NavbarPublic.vue
@@ -0,0 +1,12 @@
+<template>
+    <header class="py-3 px-3 flex justify-between items-center">
+      <router-link to="/login">
+        <icon name="user"></icon>
+      </router-link>
+        <icon name="search"/>
+    </header>
+  </template>
+  
+  <script>
+  export default {};
+  </script>
