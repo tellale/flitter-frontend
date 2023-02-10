60b405b0cb75fdd009141b5969ef56d3a66660ed
Isela Alarcón <isela.v.alarcon@gmail.com>
2023-02-10 17:24:11 +0100

    fix image avatar

--- src/components/tweetGet.vue
+++ src/components/tweetGet.vue
@@ -5,7 +5,10 @@
     class="w-full p-4 border-b hover:bg-ligther flex"
   >
     <div class="flex-none mr-4">
-      <img src="https://i.pravatar.cc/300" class="h-16 w-16 rounded-full flex-none">
+      <img
+        :src="tweet.postedBy.avatar"
+        class="h-16 w-16 rounded-full flex-none"
+      />
     </div>
     <div class="w-full">
       <div class="flex flex-wrap items-center text-left w-full">
@@ -20,22 +23,28 @@
       </div>
       <p class="text-left py-3">{{ tweet.text }}</p>
 
-        <div class="flex items-center place-content-end text-md px-4 text-grey">
-            <div class="mr-10">
-                <button v-show="isAuth" class="rounded-full text-lightblue border border-lightblue py-1 px-4 hover:text-white hover:bg-lightblue">
-                    Seguir
-                </button>
-            </div>
-            <button v-show="isAuth" @click="addLike(tweet._id)" class="flex items-center place-content-end hover:text-lightblue">
-                <font-awesome-icon icon="fa-regular fa-heart" class="mr-3" />
-                <p>{{ tweet.likes.length }}</p>
-            </button>
-            <div v-show="!isAuth" class="flex items-center place-content-end">
-                <font-awesome-icon icon="fa-regular fa-heart" class="mr-3" />
-                <p>{{ tweet.likes.length }}</p>
-            </div>
-            
+      <div class="flex items-center place-content-end text-md px-4 text-grey">
+        <div class="mr-10">
+          <button
+            v-show="isAuth"
+            class="rounded-full text-lightblue border border-lightblue py-1 px-4 hover:text-white hover:bg-lightblue"
+          >
+            Seguir
+          </button>
         </div>
+        <button
+          v-show="isAuth"
+          @click="addLike(tweet._id)"
+          class="flex items-center place-content-end hover:text-lightblue"
+        >
+          <font-awesome-icon icon="fa-regular fa-heart" class="mr-3" />
+          <p>{{ tweet.likes.length }}</p>
+        </button>
+        <div v-show="!isAuth" class="flex items-center place-content-end">
+          <font-awesome-icon icon="fa-regular fa-heart" class="mr-3" />
+          <p>{{ tweet.likes.length }}</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -49,7 +58,7 @@ export default {
   setup() {
     const store = useTweetsStore();
 
-    const isAuth = ref(true)
+    const isAuth = ref(true);
 
     // const getTweets = computed(() => {
     //     return store.getTweets;
@@ -64,14 +73,14 @@ export default {
     // });
 
     const addLike = async (tweetId: number) => {
-        store.likeTweet(tweetId) 
-    } 
+      store.likeTweet(tweetId);
+    };
 
     return {
       //getTweets,
       tweets,
       addLike,
-      isAuth
+      isAuth,
     };
   },
 };
