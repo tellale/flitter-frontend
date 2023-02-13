24b61a719374099c6c67a1a6c448dd192ec87e03
Isela Alarcón <isela.v.alarcon@gmail.com>
2023-02-13 23:34:26 +0100

    Fix creation view

--- src/components/tweetWrite.vue
+++ src/components/tweetWrite.vue
@@ -1,89 +1,87 @@
 <template>
-    <div>
-        <div class="px-5 py-3 flex">
-            <form @submit.prevent="handleSubmit" class="w-full px-4 relative">
-                <div class="flex justify-between my-4">
-                    <button 
-                        @click="click" 
-                        class="bg-none text-lightblue hover:text-darkblue hover:underline"
-                        >
-                            Cancelar
-                    </button>
-                    <button
-                        class="h-10 px-4 bottom-0 right-0 rounded-full text-white font-semibold bg-lightblue hover:bg-darkblue focus:outline-none">
-                            Fleet
-                    </button>
-                </div>
-                <div class="flex justify-between">
-                    <img 
-                        src="@/assets/profile-pic.jpg"
-                        class="w-12 h-12 rounded-full object-cover mr-4"
-                    />
-                    <textarea 
-                        v-model="newTweet" 
-                        placeholder="Que esta pasando?"
-                        class="w-full mt-3 pb-3 focus:outline-none">
-                    </textarea>
-                </div>
-                <!--Items the emoji y picture
-                     <div class="flex items-center">
-                <font-awesome-icon icon="fa-regular fa-image" class="text-lg text-blue mr-4" />
-                <font-awesome-icon icon="fa-regular fa-face-smile" class="text-lg text-blue mr-4" />
-            </div> -->
-                <p class="text-lightblue text-left text-sm">Maximo 256 caracteres</p>
-            </form>
+  <div>
+    <div class="px-5 py-3 flex">
+      <form @submit.prevent="handleSubmit" class="w-full px-4 relative">
+        <div class="flex justify-between my-4">
+          <button
+            @click="click"
+            class="bg-none text-lightblue hover:text-darkblue hover:underline"
+          >
+            Cancelar
+          </button>
+          <button
+            class="h-10 px-4 bottom-0 right-0 rounded-full text-white font-semibold bg-lightblue hover:bg-darkblue focus:outline-none"
+          >
+            Fleet
+          </button>
         </div>
+        <div class="flex justify-between">
+          <img
+            :src="userStore.authUser.avatar"
+            class="w-12 h-12 rounded-full object-cover mr-4"
+          />
+          <textarea
+            v-model="newTweet"
+            placeholder="Que esta pasando?"
+            class="w-full mt-3 pb-3 focus:outline-none"
+          >
+          </textarea>
+        </div>
+        <p class="text-lightblue text-left text-sm">Maximo 256 caracteres</p>
+      </form>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { ref, computed } from 'vue';
-import { useTweetsStore } from '../store/index';
-import { useRouter } from 'vue-router'
-
+import { ref, computed } from "vue";
+import { useTweetsStore } from "@/store/index";
+import { useUsersStore } from "@/store/user";
+import { onBeforeMount } from "@vue/runtime-core";
+import { useRouter } from "vue-router";
 
 export default {
-    name: 'tweetWrite',
-    setup() {
-        const store = useTweetsStore();
+  name: "tweetWrite",
+  setup() {
+    const store = useTweetsStore();
+    const userStore = useUsersStore();
 
-        const newTweet = ref('')
+    const newTweet = ref("");
+    const router = useRouter();
+    onBeforeMount(async () => await userStore.fetchAuthUser());
 
-        const router = useRouter()
-
-        const handleSubmit = async () => {
-            if (newTweet.value.length > 0) {
-                try {
-                    store.writeTweet(newTweet.value)
-                } catch (err) {
-                    console.log(err)
-                } finally {
-                    store.fetchTweets()
-                    router.push({
-                        path: '/'
-                    })
-                }
-            }
+    const handleSubmit = async () => {
+      if (newTweet.value.length > 0) {
+        try {
+          store.writeTweet(newTweet.value);
+        } catch (err) {
+          console.log(err);
+        } finally {
+          store.fetchTweets();
+          router.push({
+            path: "/",
+          });
         }
+      }
+    };
 
-        const tweets = computed(() => {
-            return store.tweets;
-        })
+    const tweets = computed(() => {
+      return store.tweets;
+    });
 
+    const click = () => {
+      router.push({
+        path: "/",
+      });
+    };
 
-        const click = () => {
-            router.push({
-                path: '/'
-            })
-        }
-
-        return {
-            tweets,
-            newTweet,
-            handleSubmit,
-            click
-        }
-
-    }
-}
+    return {
+      userStore,
+      tweets,
+      newTweet,
+      handleSubmit,
+      click,
+    };
+  },
+};
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,27 +6,39 @@
       <flitterHeader />
       <tweetGet />
       <div class="paginationNav">
-            <nav>
-              <ul class="pagination items-center justify-content-center">
-                <li class="page-item">
-                  <a class="text-lightblue p-3 hover:text-grey" @click="getPreviousPage()" >Anterior</a>
-                </li>
-                <li 
-                  v-for="page in totalPages" 
-                  :key="page" 
-                  @click="getDataPage(page)" 
-                  v-bind:class="isActive(page)" 
-                  class="text-lightblue">
-                    <a class="text-white bg-lightblue p-2 rounded-2 px-3 hover:bg-grey" >{{ page }}</a>
-                </li>
-                <li class="text-lightblue">
-                  <a class="text-lightblue p-3 hover:text-grey" @click="getNextPage()"  >Siguiente</a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-      
-      <div v-if="isAuth" class="fixed z-10 bottom-20 right-5">
+        <nav>
+          <ul class="pagination items-center justify-content-center">
+            <li class="page-item">
+              <a
+                class="text-lightblue p-3 hover:text-grey"
+                @click="getPreviousPage()"
+                >Anterior</a
+              >
+            </li>
+            <li
+              v-for="page in totalPages"
+              :key="page"
+              @click="getDataPage(page)"
+              v-bind:class="isActive(page)"
+              class="text-lightblue"
+            >
+              <a
+                class="text-white bg-lightblue p-2 rounded-2 px-3 hover:bg-grey"
+                >{{ page }}</a
+              >
+            </li>
+            <li class="text-lightblue">
+              <a
+                class="text-lightblue p-3 hover:text-grey"
+                @click="getNextPage()"
+                >Siguiente</a
+              >
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <div v-if="userStore.isAuth" class="fixed z-10 bottom-20 right-5">
         <button @click="click">
           <font-awesome-icon
             icon="fa-solid fa-plus"
@@ -43,13 +55,12 @@ import tweetGet from "@/components/tweetGet.vue";
 
 import flitterHeader from "@/components/flitterHeader.vue";
 import { useRouter } from "vue-router";
-import { useTweetsStore } from "../store/index";
-import { onMounted } from '@vue/runtime-core';
+import { useTweetsStore } from "@/store/index";
+import { useUsersStore } from "@/store/user";
+
+import { onBeforeMount, onMounted } from "@vue/runtime-core";
 import { ref, computed } from "vue";
-import Tweet from '@/interfaces/Tweets';
-
-
-
+import Tweet from "@/interfaces/Tweets";
 
 export default {
   name: "HomeView",
@@ -58,19 +69,19 @@ export default {
     flitterHeader,
   },
   setup() {
-    const store = useTweetsStore()
-    const isAuth = ref(false)
-
+    const store = useTweetsStore();
+    const userStore = useUsersStore();
+    onBeforeMount(async () => await userStore.fetchAuthUser());
     onMounted(() => {
-      store.fetchTweets(0, 10)
-    })
-    
-    const router = useRouter()
+      store.fetchTweets(0, 10);
+    });
+
+    const router = useRouter();
     const click = () => {
       router.push({
-        path: '/write-tweet'
-      })
-    }
+        path: "/write-tweet",
+      });
+    };
 
     //PAGINACIÓN
     const elementsPerPage = 10;
@@ -79,47 +90,46 @@ export default {
     let totalPages = computed(() => {
       const pages = Math.ceil(store.getTweetsLength / elementsPerPage);
       return pages;
-    })
+    });
 
     const getDataPage = (page: number) => {
       actualPage = page;
       paginatedData.value = [];
-      let ini = (page * elementsPerPage) - elementsPerPage;
-      let fin = (page * elementsPerPage);
-      paginatedData.value = store.getTweets.slice(ini,fin)
-    }
+      let ini = page * elementsPerPage - elementsPerPage;
+      let fin = page * elementsPerPage;
+      paginatedData.value = store.getTweets.slice(ini, fin);
+    };
 
     let paginatedData = ref<Tweet[]>([]);
 
     const getPreviousPage = () => {
-      if(actualPage > 1){
+      if (actualPage > 1) {
         actualPage--;
       }
       getDataPage(actualPage);
-    }
-   
+    };
+
     const getNextPage = () => {
-      if(actualPage < totalPages.value){
+      if (actualPage < totalPages.value) {
         actualPage++;
       }
       getDataPage(actualPage);
-    }
+    };
 
-    const isActive = (page: number) =>{
-      return page == actualPage ? 'active' : '';
-    }
+    const isActive = (page: number) => {
+      return page == actualPage ? "active" : "";
+    };
 
     return {
-      isAuth, 
+      userStore,
       click,
       totalPages,
       getDataPage,
       getPreviousPage,
       getNextPage,
       paginatedData,
-      isActive
-,
-    }
+      isActive,
+    };
   },
-}
+};
 </script>
